1232087ca8f8a6d10d12ad2e3f50ae1ad283f4d2
Shailesh Gaikwad <shailesh.gaikwad078@gmail.com>
2023-08-06 15:52:09 +0530

    sam peoject

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './New.css';
+
+function App() {
+  return (
+    <center>
+      <div>
+        <h3 className='tRed'>Welcome</h3>
+        <h3 className='tRed'>Welcome</h3>
+      </div>
+    </center>
+  );
+}
+
+export default App;
